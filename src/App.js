a15cb241432a534c9d6390861eed5d5513ec04bb
Meet Master <meetmaster16@gmail.com>
2022-01-27 23:58:54 +0530

    Solved delopment issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { useContext, useEffect, useState } from "react";
 import "./App.css";
 import Calculator from "./components/Calculator";
 
 function App() {
-  // const [themeMode, setThemeMode] = useContext(ContextTheme);
-  // const [theme, setTheme] = useState("#000");
-  // useEffect(() => {
-  //   document.body.style.backgroundColor = theme;
-  // });
-
   return (
     <div className="App">
       <Calculator />
